da23de886eb2ade95f0732b04ea32370744997a5
Yan-Fa Li <yanfali@gmail.com>
2014-09-29 19:57:33 -0700

    Use fork
    - initialize instance of Tvdb
    - rename source file

--- go-tvdb-cli.go
+++ go-tvdb-cli.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/garfunkel/go-tvdb"
+	"github.com/yanfali/go-tvdb"
 )
 
 func main() {
-	results, err := tvdb.SearchSeries(os.Args[1], 10)
+	myTvdb := tvdb.NewTvdb()
+	results, err := myTvdb.SearchSeries(os.Args[1], 10)
 	if err != nil {
 		fmt.Errorf("error", err)
 	}
